3cd4a0d563d170bd2eec2c62705931fdc07ee1f5
Abby Christens <achristens@gmail.com>
2017-09-21 18:43:27 -0400

    Finds highest and lowest rolls

--- lowest_and_highest.rb
+++ lowest_and_highest.rb
@@ -1,6 +1,6 @@
 list_answers = []
 
-10.times do
+5.times do
     results = Random.rand(1..6)
     list_answers << results
 end
@@ -10,3 +10,6 @@ list_sorted = list_answers.sort!
 list_sorted.each do |value|
   puts "The result of your roll is #{value}."
 end
+
+puts "The lowest number is #{list_sorted.first}."
+puts "The highest number is #{list_sorted.last}."
